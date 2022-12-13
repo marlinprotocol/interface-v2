24276d6b612a02ca9c45b53866448ee72a6d0b8c
Souvik Mishra <mishra.souvik911@gmail.com>
2022-12-13 17:18:26 +0530

    improvement: created a new modal component and added in walletAuth

---
+++ src/lib/modal/Modal.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { scale } from 'svelte/transition';
+	import { quadInOut } from 'svelte/easing';
+
+	const classesDefault =
+		'items-center z-50 rounded bg-white dark:bg-dark-400 p-8 w-full max-w-lg shadow';
+	const titleClassesDefault = 'text-lg font-bold pb-4 flex justify-between align-center';
+	const actionsClassesDefault = 'flex w-full justify-end';
+	const background = 'bg-black/20 z-10 fixed top-0 left-0 w-full h-full';
+
+	export let value: Boolean;
+	export let classes = classesDefault;
+	export let titleClasses = titleClassesDefault;
+	export let actionsClasses = actionsClassesDefault;
+	export let persistent = false;
+
+	export let transitionProps = { duration: 300, easing: quadInOut, delay: 50 };
+</script>
+
+{#if value}
+	<div class="fixed w-full h-full top-0 left-0 z-30">
+		<div class={background} on:click={() => !persistent && (value = false)} />
+		<div class="h-full w-full absolute flex items-center justify-center">
+			<div in:scale={transitionProps} class={classes}>
+				<div class={titleClasses}>
+					<slot name="title" />
+					<button type="button" on:click={() => (value = false)}>
+						<img src="/images/closemodal.svg" alt="close modal" />
+					</button>
+				</div>
+				<slot />
+				<div class={actionsClasses}>
+					<slot name="actions" />
+				</div>
+			</div>
+		</div>
+	</div>
+{/if}
--- src/lib/wallet-auth/WalletAuth.svelte
+++ src/lib/wallet-auth/WalletAuth.svelte
@@ -3,6 +3,7 @@
 	import { provider as s_provider, address as s_address, networkId } from '../stores/provider';
 	import { onMount } from 'svelte';
 	import { providers } from 'ethers';
+	import Modal from '$lib/modal/Modal.svelte';
 
 	export let allowedNetworks = [import.meta.env.VITE_NETWORK_ID as string];
 	let networkNames = {
@@ -112,7 +113,7 @@
 
 {#if walletType !== 'NONE'}
 	<button
-		class="flex flex-row justify-center space-x-2 bg-transparent outline outline-[1px] outline-primary rounded-lg p-2"
+		class="flex flex-row justify-center space-x-3 bg-white rounded-lg px-4 py-2 shadow-sm hover:shadow-lg transition-shadow duration-000"
 		on:click={() => (logoutModal = !logoutModal)}
 	>
 		{#if walletType === 'METAMASK'}
@@ -123,71 +124,31 @@
 			<img src="/images/wallet.svg" alt="unrecognized wallet icon" width="35" class="my-auto" />
 		{/if}
 		<div class="info flex flex-col">
-			<span class="font-[Poppins] font-bold text-primary"
+			<span class="font-[Poppins] font-bold text-primary text-sm"
 				>{networkNames[$networkId] || 'UNKNOWN'}</span
 			>
-			<span class="font-[Poppins] font-normal text-primary text-xs"
-				>{address.slice(0, 7)}...{address.slice(address.length - 5)}</span
+			<span class="font-[Poppins] font-normal text-primary text-[11px]"
+				>{address.slice(0, 6)}...{address.slice(address.length - 5)}</span
 			>
 		</div>
 	</button>
 
 	{#if logoutModal}
-		<div class="fixed z-10 inset-0 overflow-y-auto">
-			<div
-				class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+		<Modal bind:value={logoutModal}>
+			<p
+				slot="title"
+				class="modal-title my-auto font-[Poppins] font-bold text-neutral-800 text-2xl"
 			>
-				<!--
-				Background overlay, show/hide based on modal state.
+				Your wallet
+			</p>
 
-				Entering: "ease-out duration-300"
-					From: "opacity-0"
-					To: "opacity-100"
-				Leaving: "ease-in duration-200"
-					From: "opacity-100"
-					To: "opacity-0"
-			-->
-				<div
-					class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-					on:click|self={() => (logoutModal = !logoutModal)}
-				/>
-
-				<!-- This element is to trick the browser into centering the modal contents. -->
-				<span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-
-				<!--
-				Modal panel, show/hide based on modal state.
-
-				Entering: "ease-out duration-300"
-					From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-					To: "opacity-100 translate-y-0 sm:scale-100"
-				Leaving: "ease-in duration-200"
-					From: "opacity-100 translate-y-0 sm:scale-100"
-					To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-			-->
-				<div
-					class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-				>
-					<div class="modal-header flex flex-row justify-between p-8 pb-0">
-						<p class="modal-title my-auto font-[Poppins] font-bold text-neutral-800 text-2xl">
-							Your wallet
-						</p>
-						<button type="button" class="modal-close" on:click={() => (logoutModal = !logoutModal)}>
-							<img src="/images/closemodal.svg" alt="close modal" />
-						</button>
-					</div>
-					<div class="modal-body my-4 font-[Poppins] font-lg font-semibold">
-						<div class="p-8 pb-4 m-auto">
-							<button
-								on:click={logout}
-								class="connect-wallet-btn bg-primary text-center text-white font-semibold p-4 w-full bg-primaryBlue rounded-lg"
-								>Logout</button
-							>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+			<button
+				slot="actions"
+				on:click={logout}
+				class="connect-wallet-btn bg-primary text-center text-white font-semibold p-4 w-full bg-primaryBlue rounded-lg"
+				>Logout</button
+			>
+		</Modal>
 	{/if}
 {:else}
 	<button
@@ -201,92 +162,35 @@
 		>
 	</button>
 	{#if connectModal}
-		<div class="fixed z-10 inset-0 overflow-y-auto">
-			<div
-				class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-			>
-				<!--
-				Background overlay, show/hide based on modal state.
+		<Modal bind:value={connectModal}>
+			<div slot="title">
+				<p class="my-auto font-[Poppins] font-medium text-neutral-800/60 text-sm">Unlock Wallet</p>
+				<p class="my-auto font-[Poppins] font-bold text-neutral-800 text-2xl">Select a provider</p>
+			</div>
+			<div class="flex flex-row justify-evenly gap-5 mt-4">
+				<div class="connect-card p-4 w-full bg-gray-100 rounded-lg h-fit">
+					<img class="mx-auto mb-4 h-16" src="/images/metamaskicon.svg" alt="metamask icon" />
+					<p class="heading pb-6 text-lg font-semibold text-center">MetaMask</p>
 
-				Entering: "ease-out duration-300"
-					From: "opacity-0"
-					To: "opacity-100"
-				Leaving: "ease-in duration-200"
-					From: "opacity-100"
-					To: "opacity-0"
-			-->
-				<div
-					class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-					on:click|self={() => (connectModal = !connectModal)}
-				/>
+					<button
+						on:click={connectMetamask}
+						disabled={!hasMetamask}
+						class:bg-gray-400={!hasMetamask}
+						class:bg-primaryBlue={hasMetamask}
+						class="connect-wallet-btn bg-primary text-center text-white font-semibold p-2 w-full h-12 rounded-lg"
+						>{#if hasMetamask}Connect{:else}Not Found{/if}</button
+					>
+				</div>
+				<div class="connect-card p-4 w-full bg-gray-100 rounded-lg h-fit">
+					<img class="mx-auto mb-4 h-16" src="/images/walleticon.svg" alt="wallet connect icon" />
+					<p class="heading pb-6 text-lg font-semibold text-center">WalletConnect</p>
 
-				<!-- This element is to trick the browser into centering the modal contents. -->
-				<span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
-
-				<!--
-				Modal panel, show/hide based on modal state.
-
-				Entering: "ease-out duration-300"
-					From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-					To: "opacity-100 translate-y-0 sm:scale-100"
-				Leaving: "ease-in duration-200"
-					From: "opacity-100 translate-y-0 sm:scale-100"
-					To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-			-->
-				<div
-					class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-				>
-					<div class="modal-header flex flex-row justify-between p-4 pb-0">
-						<div class="pt-3 pl-1">
-							<p class="my-auto font-[Poppins] font-medium text-neutral-800/60 text-sm">
-								Unlock Wallet
-							</p>
-							<p class="my-auto font-[Poppins] font-bold text-neutral-800 text-2xl">
-								Select a provider
-							</p>
-						</div>
-						<button
-							type="button"
-							class="modal-close pt-4 pl-4"
-							on:click={() => (connectModal = !connectModal)}
-						>
-							<img src="/images/closemodal.svg" alt="close modal" />
-						</button>
-					</div>
-					<div class="modal-body mt-8 mb-4 font-[Poppins] font-lg font-semibold">
-						<div class="flex flex-row justify-evenly px-4 gap-4">
-							<div class="connect-card w-full bg-gray-100 rounded-lg">
-								<img class="m-auto py-4 h-24" src="/images/metamaskicon.svg" alt="metamask icon" />
-								<p class="heading text-center">MetaMask</p>
-								<div class="p-8 pb-4 m-auto">
-									<button
-										on:click={connectMetamask}
-										disabled={!hasMetamask}
-										class:bg-gray-400={!hasMetamask}
-										class:bg-primaryBlue={hasMetamask}
-										class="connect-wallet-btn bg-primary text-center text-white font-semibold p-4 w-full rounded-lg"
-										>{#if hasMetamask}Connect{:else}Not Found{/if}</button
-									>
-								</div>
-							</div>
-							<div class="connect-card w-full bg-gray-100 rounded-lg">
-								<img
-									class="m-auto py-4 h-24"
-									src="/images/walleticon.svg"
-									alt="wallet connect icon"
-								/>
-								<p class="heading text-center">WalletConnect</p>
-								<div class="p-8 pb-4 m-auto">
-									<button
-										class="connect-wallet-btn text-center text-white font-semibold p-4 w-full bg-gray-400 rounded-lg"
-										>Coming Soon</button
-									>
-								</div>
-							</div>
-						</div>
-					</div>
+					<button
+						class="connect-wallet-btn bg-gray-400 text-center text-white font-semibold p-2 w-full h-12 rounded-lg"
+						>Coming Soon</button
+					>
 				</div>
 			</div>
-		</div>
+		</Modal>
 	{/if}
 {/if}

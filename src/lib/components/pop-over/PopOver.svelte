45faa5a744cea467a82c48f722b4be0157787f07
vcshivani <sbas1909@MSI.localdomain>
2023-01-27 18:45:39 +0400

    timer-add

--- src/lib/components/pop-over/PopOver.svelte
+++ src/lib/components/pop-over/PopOver.svelte
@@ -2,10 +2,7 @@
 	import { buttonClasses } from '../componentClasses';
 
 	const styles = {
-		dropdownContent:
-			'dropdown-content menu shadow rounded rounded-box w-56 bg-gray-600 text-primary-content',
-		title: 'py-2 px-3 bg-gray-700 rounded',
-		subtitle: 'py-2 px-3'
+		dropdownContent: 'dropdown-content menu shadow rounded rounded-box w-72 bg-base-100'
 	};
 </script>
 

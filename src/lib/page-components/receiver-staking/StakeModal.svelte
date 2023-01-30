90d59c7e1dd9406883000d77eefa5361f844aa29
Souvik Mishra <mishra.souvik911@gmail.com>
2023-01-30 18:27:10 +0530

    improvement: integrated max pond value in stake modal

--- src/lib/page-components/receiver-staking/StakeModal.svelte
+++ src/lib/page-components/receiver-staking/StakeModal.svelte
@@ -2,12 +2,13 @@
 	import FilledButton from '$lib/components/buttons/FilledButton.svelte';
 	import Modal from '$lib/components/modals/Modal.svelte';
 	import Text from '$lib/components/texts/Text.svelte';
-	import { BigNumber } from 'ethers';
+	import { walletBalance } from '$lib/data-stores/walletBalanceStore';
+	import { DEFAULT_WALLET_BALANCE } from '$lib/utils/constants/storeDefaults';
+	import { onDestroy } from 'svelte';
 	import ModalApproveButton from './sub-components/ModalApproveButton.svelte';
 	import ModalPondInput from './sub-components/ModalPondInput.svelte';
 
 	const modalFor = 'stake-modal';
-	const maxAmount = BigNumber.from('20000000000000000000000');
 
 	//initial amount states
 	let inputPondAmount: number = 0;
@@ -16,6 +17,12 @@
 	//loading states
 	let approveLoading: boolean = false;
 	let submitLoading: boolean = false;
+
+	// max amount in wallet
+	let maxPondBalance = DEFAULT_WALLET_BALANCE.pond;
+	const unsubscribeWalletBalanceStore = walletBalance.subscribe((value) => {
+		maxPondBalance = value.pond;
+	});
 
 	const handleApproveClick = async () => {
 		// TODO: call approve function
@@ -36,15 +43,19 @@
 		}, 3000);
 	};
 
+	onDestroy(unsubscribeWalletBalanceStore);
+
 	//button states
 	//considering max amount is required
 	$: approveDisabled =
-		!!maxAmount &&
-		(inputPondAmount <= 0 || !maxAmount.gte(inputPondAmount) || inputPondAmount <= approvedPond);
+		!!maxPondBalance &&
+		(inputPondAmount <= 0 ||
+			!maxPondBalance.gte(inputPondAmount) ||
+			inputPondAmount <= approvedPond);
 	$: pondDisabledText =
-		!!inputPondAmount && !maxAmount.gte(inputPondAmount) ? 'Insufficient POND' : '';
+		!!inputPondAmount && !maxPondBalance.gte(inputPondAmount) ? 'Insufficient POND' : '';
 	$: submitEnable =
-		!!inputPondAmount && inputPondAmount <= approvedPond && maxAmount.gte(inputPondAmount);
+		!!inputPondAmount && inputPondAmount <= approvedPond && maxPondBalance.gte(inputPondAmount);
 </script>
 
 <div>
@@ -60,7 +71,7 @@
 				title={'POND'}
 				tooltipText={'Some text here'}
 				bind:inputAmount={inputPondAmount}
-				{maxAmount}
+				maxAmount={maxPondBalance}
 				maxAmountText={'Balance'}
 			>
 				<ModalApproveButton

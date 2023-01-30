bae53a8fbc46f652341b656d83207a34f46b2a4d
Souvik Mishra <mishra.souvik911@gmail.com>
2023-01-30 17:26:15 +0530

    improvement: moved contract data to a separate store

--- src/lib/controllers/contractController.ts
+++ src/lib/controllers/contractController.ts
@@ -1,3 +1,4 @@
+import { contractStore } from '$lib/data-stores/contractStore';
 import ENVIRONMENT from '$lib/environments/environment';
 import { GET_OPTIONS } from '$lib/utils/constants/constants';
 import { fetchHttpData } from '$lib/utils/helpers/httpHelper';
@@ -5,9 +6,7 @@ import { fetchHttpData } from '$lib/utils/helpers/httpHelper';
 export async function getContractDetails() {
 	const url = ENVIRONMENT.public_contract_details_url;
 	const options = GET_OPTIONS;
-
 	// TODO: add type for contractDetails
 	const contractDetails = await fetchHttpData(url, options);
-	console.log('contractDetails', contractDetails);
-	return contractDetails;
+	contractStore.set(contractDetails);
 }
--- src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { getContractDetails } from '$lib/controllers/contractController';
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async () => {
-	return {
-		contract: await getContractDetails()
-	};
-}) satisfies LayoutServerLoad;

1868c8ad17b5b39400e5e230b28531bdf913b174
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-01 17:35:22 +0530

    refactor: removed duplicate function declaration

--- src/lib/data-stores/chainProviderStore.ts
+++ src/lib/data-stores/chainProviderStore.ts
@@ -1,4 +1,3 @@
-import ENVIRONMENT from '$lib/environments/environment';
 import type { ChainStore } from '$lib/types/storeTypes';
 import { DEFAULT_CHAIN_STORE } from '$lib/utils/constants/storeDefaults';
 import { writable, type Writable } from 'svelte/store';
@@ -11,8 +10,4 @@ export const chainStore: Writable<ChainStore> = writable(DEFAULT_CHAIN_STORE);
  */
 export function resetChainProviderStore() {
 	chainStore.set(DEFAULT_CHAIN_STORE);
-}
-
-export function isValidNetwork(id: number): boolean {
-	return ENVIRONMENT.valid_chain_ids.includes(id);
 }

75e31fd591487cd84693bfe69fe43d41387c1133
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-03 17:33:29 +0530

    refactor: removed the disconnect function as resetting the wallet provider is more efficient

--- src/lib/page-components/header/sub-components/DisconnectWalletModal.svelte
+++ src/lib/page-components/header/sub-components/DisconnectWalletModal.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
 	import FilledButton from '$lib/atoms/buttons/FilledButton.svelte';
 	import Modal from '$lib/atoms/modals/Modal.svelte';
-	import { resetChainProviderStore } from '$lib/data-stores/chainProviderStore';
-	import { resetWalletBalanceStore } from '$lib/data-stores/walletBalanceStore';
 	import { resetWalletProviderStore } from '$lib/data-stores/walletProviderStore';
 
 	const modalFor = 'disconnect-wallet-modal';
-
-	function disconnectWallet() {
-		resetWalletProviderStore();
-		resetWalletBalanceStore();
-		resetChainProviderStore();
-		console.log('!! Wallet disconnected !!');
-	}
 </script>
 
 <Modal {modalFor}>
 	<svelte:fragment slot="title">Disconnect Wallet</svelte:fragment>
 	<svelte:fragment slot="content">Are you sure you want to disconnect your wallet?</svelte:fragment>
 	<svelte:fragment slot="action-buttons">
-		<FilledButton onclick={disconnectWallet} styleClass={'w-full'}>Disconnect</FilledButton>
+		<FilledButton onclick={() => resetWalletProviderStore()} styleClass={'w-full'}
+			>Disconnect</FilledButton
+		>
 	</svelte:fragment>
 </Modal>

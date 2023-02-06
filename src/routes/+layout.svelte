3459313cf74aaf4dbede6ae563cfa4da657b628e
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-06 12:51:14 +0530

    improvement: added font poppins and orbitron

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,16 @@
 	});
 </script>
 
-<main class="text-center w-full">
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Orbitron&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main class="text-center w-full font-poppins">
 	<Header />
 	<slot />
 </main>

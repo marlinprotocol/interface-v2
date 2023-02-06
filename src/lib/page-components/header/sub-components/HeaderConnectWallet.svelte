e65de5e00710b0fe456fc3e2a966544c626ebf06
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-06 13:42:42 +0530

    improvement: added short wallet address to header button

--- src/lib/page-components/header/sub-components/HeaderConnectWallet.svelte
+++ src/lib/page-components/header/sub-components/HeaderConnectWallet.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
 	import { buttonClasses } from '$lib/atoms/componentClasses';
-	import { connected } from '$lib/data-stores/walletProviderStore';
+	import { connected, walletStore } from '$lib/data-stores/walletProviderStore';
 	import DisconnectWalletModal from '$lib/page-components/header/sub-components/DisconnectWalletModal.svelte';
 	import ConnectWalletModal from '$lib/page-components/header/sub-components/ConnectWalletModal.svelte';
 	import lock from 'svelte-awesome/icons/lock';
 	import Icon from '$lib/atoms/icons/Icon.svelte';
+
+	$: shortAddress =
+		$walletStore.address.slice().substring(0, 6) +
+		'...' +
+		$walletStore.address.slice().substring(38, 42);
 </script>
 
 {#if $connected}
-	<label for="disconnect-wallet-modal" class={`${buttonClasses.outlined} flex gap-2`}>
-		<img src="./images/metamaskicon.svg" alt="Metamask Logo" width="24" />
-		Disconnect Wallet
+	<label
+		for="disconnect-wallet-modal"
+		class="bg-base-100 cursor-pointer text-primary text-sm font-semibold rounded-lg shadow-sm flex gap-2 pl-3 pr-4 py-1 items-center border-primary border"
+	>
+		<img src="./images/wallet-connected.svg" alt="Metamask Logo" />
+		{shortAddress}
 	</label>
 	<DisconnectWalletModal />
 {:else}

9bffe8291798aaf6dc6cc18421325b0f0ea34965
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-16 12:37:55 +0530

    improvement: added extra prop for input card

--- src/lib/atoms/cards/InputCard.svelte
+++ src/lib/atoms/cards/InputCard.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	export let styles: string = '';
 	const cardStyle = 'rounded-lg w-full bg-base-200 p-4 ';
 </script>
 
-<div class={cardStyle}>
+<div class={cardStyle + styles}>
 	<slot />
 </div>

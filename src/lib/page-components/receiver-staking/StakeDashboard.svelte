1c9dc54cde1708ff2b269e9c68e07d09c840c6e7
Souvik Mishra <mishra.souvik911@gmail.com>
2023-02-17 11:11:15 +0530

    improvement: added cta to container card when wallet not connected

--- src/lib/page-components/receiver-staking/StakeDashboard.svelte
+++ src/lib/page-components/receiver-staking/StakeDashboard.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+	import FilledButton from '$lib/atoms/buttons/FilledButton.svelte';
 	import ContainerCard from '$lib/atoms/cards/ContainerCard.svelte';
 	import { buttonClasses } from '$lib/atoms/componentClasses';
+	import Icon from '$lib/atoms/icons/Icon.svelte';
 	import Text from '$lib/atoms/texts/Text.svelte';
+	import { connected } from '$lib/data-stores/walletProviderStore';
 	import StakedData from '$lib/page-components/receiver-staking/StakedData.svelte';
 	import StakeModal from '$lib/page-components/receiver-staking/StakeModal.svelte';
 	import UnstakeModal from '$lib/page-components/receiver-staking/UnstakeModal.svelte';
+	import lock from 'svelte-awesome/icons/lock';
 
 	const styles = {
 		buttonsGroup: 'flex gap-5 items-center justify-center w-full',
@@ -16,15 +20,22 @@
 	<ContainerCard>
 		<Text variant="h3" text="Receiver Staking Portal" styleClass="text-center" />
 		<StakedData />
-		<div class={styles.buttonsGroup}>
-			<div class={styles.buttonWrapper}>
-				<label for="stake-modal" class={`${buttonClasses.filled} w-full`}>Stake</label>
+		{#if $connected}
+			<div class={styles.buttonsGroup}>
+				<div class={styles.buttonWrapper}>
+					<label for="stake-modal" class={`${buttonClasses.filled} w-full`}>Stake</label>
+				</div>
+				<div class={styles.buttonWrapper}>
+					<label for="unstake-modal" class={`${buttonClasses.outlined} w-full`}>Unstake</label>
+				</div>
 			</div>
-			<div class={styles.buttonWrapper}>
-				<label for="unstake-modal" class={`${buttonClasses.outlined} w-full`}>Unstake</label>
-			</div>
-		</div>
-		<StakeModal />
-		<UnstakeModal />
+			<StakeModal />
+			<UnstakeModal />
+		{:else}
+			<label for="connect-wallet-modal" class={`${buttonClasses.filled} flex gap-1`}>
+				<Icon data={lock} size={20} iconColorClass="text-white" />
+				Connect Wallet
+			</label>
+		{/if}
 	</ContainerCard>
 </div>

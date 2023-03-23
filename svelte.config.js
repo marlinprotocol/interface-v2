c1ed21f09723a6af27ca27cba7d9d3877fccbf58
Souvik Mishra <mishra.souvik911@gmail.com>
2023-03-23 16:51:05 +0530

    misc: added vercel deployment essentials

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };

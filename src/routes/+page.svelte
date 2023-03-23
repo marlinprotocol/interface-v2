8b4bd61512278992d4fe5fd3b1e357fdeabde59b
Souvik Mishra <mishra.souvik911@gmail.com>
2023-03-23 16:35:22 +0530

    misc: commit to trigger build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
 	import NetworkPrompt from '$lib/components/prompts/NetworkPrompt.svelte';
 	import { chainStore } from '$lib/data-stores/chainProviderStore';
 	import StakeDashboard from '$lib/page-components/receiver-staking/StakeDashboard.svelte';
+	import Test from '$lib/page-components/receiver-staking/Test.svelte';
 </script>
 
 <div class="py-10">
 	{#if $chainStore.isValidChain}
 		<StakeDashboard />
+		<!-- <Test /> -->
 	{:else}
 		<NetworkPrompt />
 	{/if}

da5ae6933e73c9ddbb2a0d28de78aff9166735ea
vcshivani <shivani.bansal0020@gmail.com>
2023-04-10 16:58:35 +0400

    bigFixes:disable-cancel-button-when-nothing-mpond-table--arrow-button-enable-in-input-field

--- src/lib/components/inputs/AmountInputWithMaxButton.svelte
+++ src/lib/components/inputs/AmountInputWithMaxButton.svelte
@@ -43,12 +43,16 @@
 				placeholder="0.00"
 				autocomplete="off"
 				on:keydown={(e) => {
-					// user can only enter numbers and one decimal point
+					// user can only enter numbers and one decimal point, arrows, backspace, delete
 					if (
 						(e.key >= '0' && e.key <= '9') ||
 						(e.key == '.' && !inputAmountString.includes('.')) ||
 						e.key == 'Backspace' ||
-						e.key == 'Delete'
+						e.key == 'Delete' ||
+						e.key == 'ArrowLeft' ||
+						e.key == 'ArrowRight' ||
+						e.key == 'ArrowUp' ||
+						e.key == 'ArrowDown'
 					) {
 						return true;
 					} else {

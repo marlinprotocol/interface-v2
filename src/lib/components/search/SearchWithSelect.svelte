52722b898e033fe18cc56da313604c2479493c7e
Souvik Mishra <mishra.souvik911@gmail.com>
2023-05-29 16:23:25 +0530

    improvement: close the suggestion dropdown if user deletes search input

--- src/lib/components/search/SearchWithSelect.svelte
+++ src/lib/components/search/SearchWithSelect.svelte
@@ -26,6 +26,11 @@
 	const handleSearch = async (event: any) => {
 		const input = event.target as HTMLInputElement;
 		searchValue = input.value;
+		// close suggestions if search is empty
+		if (searchValue === '') {
+			showSuggestions = false;
+			return;
+		}
 		suggestions = dataList.filter((item) =>
 			item
 				.toString()

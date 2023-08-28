fb1ab4142c2b933e37bc989f5fb39ba89aed644a
Souvik Mishra <mishra.souvik911@gmail.com>
2023-08-28 23:23:15 +0530

    minor ui change to make icon centered in toast

--- src/lib/atoms/toast/Toast.svelte
+++ src/lib/atoms/toast/Toast.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
-	import type { ToastModel } from '$lib/types/componentTypes';
 	import Icon from '$lib/atoms/icons/Icon.svelte';
 	import { dismissToast, toastsStore } from '$lib/data-stores/toastStore';
 	import { fade, slide } from 'svelte/transition';
-
-	let toasts: ToastModel[];
-	toastsStore.subscribe((value) => {
-		toasts = value;
-	});
 
 	const baseClass =
 		'alert w-fit shadow-lg font-medium flex items-start justify-start gap-1 px-4 py-2 rounded flex-row';
 </script>
 
-{#if toasts}
+{#if $toastsStore.length > 0}
 	<div class="toast toast-top toast-end items-end z-[9999]">
-		{#each toasts as toast (toast.id)}
+		{#each $toastsStore as toast (toast.id)}
 			<div
 				in:slide
 				out:fade
@@ -23,7 +17,7 @@
 				on:keydown={() => dismissToast(toast.id)}
 				class={`${toast.className} ${baseClass}`}
 			>
-				<div class={'h-5 w-5'}>
+				<div class={'h-5 flex items-center justify-center w-5'}>
 					<Icon iconColorClass={toast.iconColor} data={toast.iconData} size={18} />
 				</div>
 				<span class="grow text-sm max-w-[330px] text-left">{toast.message}</span>

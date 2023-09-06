41f08c48472ce785313f78baa425b6ec681515ef
Souvik Mishra <mishra.souvik911@gmail.com>
2023-09-07 00:02:14 +0530

    nested staking and rewards page under relay route

--- src/routes/receiver-portal/+page.ts
+++
@@ -1,6 +0,0 @@
-import { RECEIVER_STAKING_URL } from '$lib/utils/constants/urls';
-import { redirect } from '@sveltejs/kit';
-
-export function load() {
-	throw redirect(308, RECEIVER_STAKING_URL);
-}

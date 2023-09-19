e4a2fbdd4f2bb5431943e915602af2477b4e1452
Souvik Mishra <mishra.souvik911@gmail.com>
2023-09-19 14:56:20 +0530

    refactor load logic into page

--- src/routes/bridge/+layout.svelte
+++ src/routes/bridge/+layout.svelte
@@ -1,45 +1,20 @@
 <script lang="ts">
 	import NetworkPrompt from '$lib/components/prompts/NetworkPrompt.svelte';
 	import {
-		getPondAndMPondBridgeAllowancesFromSubgraph,
-		getRequestedMPondForConversionFromSubgraph
-	} from '$lib/controllers/subgraphController';
-	import { initializeBridgeStore } from '$lib/data-stores/bridgeStore';
-	import {
 		chainStore,
 		setAllowedChainsStore,
 		allowedChainsStore
 	} from '$lib/data-stores/chainProviderStore';
-	import { contractAddressStore } from '$lib/data-stores/contractStore';
 	import { environment } from '$lib/data-stores/environment';
-	import { connected, walletStore } from '$lib/data-stores/walletProviderStore';
-	import { modifyAllowancesData } from '$lib/utils/data-modifiers/subgraphModifier';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		setAllowedChainsStore(environment.supported_chains.bridge);
 	});
 
-	async function init() {
-		const [allowancesData, requestedMPond] = await Promise.all([
-			getPondAndMPondBridgeAllowancesFromSubgraph(
-				$walletStore.address,
-				$contractAddressStore.BRIDGE
-			),
-			getRequestedMPondForConversionFromSubgraph($walletStore.address)
-		]);
-
-		const allowances = modifyAllowancesData(allowancesData);
-		initializeBridgeStore(allowances, requestedMPond);
-	}
-
 	$: chainSupported = $chainStore.chainId
 		? $allowedChainsStore.includes($chainStore.chainId)
 		: true;
-
-	$: if ($connected && $chainStore.chainId && chainSupported) {
-		init();
-	}
 </script>
 
 <svelte:head>
--- src/routes/bridge/+page.svelte
+++ src/routes/bridge/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
+	import {
+		getPondAndMPondBridgeAllowancesFromSubgraph,
+		getRequestedMPondForConversionFromSubgraph
+	} from '$lib/controllers/subgraphController';
+	import { initializeBridgeStore } from '$lib/data-stores/bridgeStore';
+	import { chainStore } from '$lib/data-stores/chainProviderStore';
+	import { contractAddressStore } from '$lib/data-stores/contractStore';
+	import { connected, walletStore } from '$lib/data-stores/walletProviderStore';
 	import BridgeDashboard from '$lib/page-components/bridge/BridgeDashboard.svelte';
+	import { modifyAllowancesData } from '$lib/utils/data-modifiers/subgraphModifier';
+
+	async function init() {
+		const [allowancesData, requestedMPond] = await Promise.all([
+			getPondAndMPondBridgeAllowancesFromSubgraph(
+				$walletStore.address,
+				$contractAddressStore.BRIDGE
+			),
+			getRequestedMPondForConversionFromSubgraph($walletStore.address)
+		]);
+
+		const allowances = modifyAllowancesData(allowancesData);
+		initializeBridgeStore(allowances, requestedMPond);
+	}
+
+	$: if ($connected && $chainStore.chainId) {
+		init();
+	}
 </script>
 
 <div class="py-4">

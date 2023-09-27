b13bba766afc35d6304db447d54a9653e96136aa
Souvik Mishra <mishra.souvik911@gmail.com>
2023-09-27 19:29:39 +0530

    add linea to mainnet environment

--- src/lib/environments/environment.mainnet.ts
+++ src/lib/environments/environment.mainnet.ts
@@ -1,14 +1,15 @@
 import { ARB_ONE } from '$lib/chains/arbOne';
 import type { Environment } from '$lib/types/environmentTypes';
+import { LINEA } from '$lib/chains/linea';
 
 export const ENVIRONMENT_MAINNET: Environment = {
 	environment_name: 'mainnet',
 	production: true,
-	valid_chains: { 42161: ARB_ONE },
+	valid_chains: { 42161: ARB_ONE, 59272: LINEA },
 	default_chain_id: 42161,
 	supported_chains: {
 		bridge: [42161],
-		oyster: [42161],
+		oyster: [42161, 59272],
 		receiver: [42161]
 	},
 	dapp_url: 'https://arb1.marlin.org',

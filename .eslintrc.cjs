06edb2579e78c67e2c5c4b85c689125e69338145
Souvik Mishra <mishra.souvik911@gmail.com>
2023-10-16 12:20:18 +0530

    add vitest and example test file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,29 +7,26 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended'
 	],
 	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
 	parserOptions: {
-		project: './tsconfig.json',
 		extraFileExtensions: ['.svelte'],
 		sourceType: 'module',
 		ecmaVersion: 2020
 	},
-	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			// Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	],
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
 	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
 	rules: {
 		eqeqeq: ['error', 'always'],
 		'no-mixed-spaces-and-tabs': ['off']

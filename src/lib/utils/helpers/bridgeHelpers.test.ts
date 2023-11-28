e05ebfb4191eefb0450debcc40d80d1ed867c384
Souvik Mishra <mishra.souvik911@gmail.com>
2023-11-28 13:24:46 +0530

    add bridge helper tests

---
+++ src/lib/utils/helpers/bridgeHelpers.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from 'vitest';
+import { getAmountPrecision } from './bridgeHelpers';
+
+describe('getAmountPrecision', () => {
+	test('should return the precision for pond', () => {
+		expect(getAmountPrecision('pond')).toBe(2);
+	});
+	test('should return the precision for mPond', () => {
+		expect(getAmountPrecision('mPond')).toBe(6);
+	});
+	test('should return the default precision for an unknown token', () => {
+		expect(getAmountPrecision('unknown')).toBe(4);
+	});
+});

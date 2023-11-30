81d8ca5e874f9ff801818fc69bd44d81ca1d7249
Souvik Mishra <mishra.souvik911@gmail.com>
2023-11-30 17:47:11 +0530

    add @ts-nocheck to suppress errors

--- src/lib/utils/helpers/bridgeHelpers.test.ts
+++ src/lib/utils/helpers/bridgeHelpers.test.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { describe, expect, test } from 'vitest';
 import { getAmountPrecision } from './bridgeHelpers';
 

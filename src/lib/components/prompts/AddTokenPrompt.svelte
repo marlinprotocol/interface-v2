9f4b4aa41a8b089ff38c9067626908c6b1ca8f3a
Souvik Mishra <mishra.souvik911@gmail.com>
2024-01-31 12:45:59 +0530

    add check for valid chain and toast for events

--- src/lib/components/prompts/AddTokenPrompt.svelte
+++ src/lib/components/prompts/AddTokenPrompt.svelte
@@ -1,47 +1,63 @@
 <script lang="ts">
 	import Button from '$lib/atoms/buttons/Button.svelte';
-	import { chainConfigStore } from '$lib/data-stores/chainProviderStore';
+	import { chainConfigStore, chainStore } from '$lib/data-stores/chainProviderStore';
 	import { web3WalletStore } from '$lib/data-stores/walletProviderStore';
 	import type { Eip1193Provider } from 'ethers';
 	import { staticImages } from '../images/staticImages';
 	import type { TokenMetadata } from '$lib/types/environmentTypes';
+	import { addToast } from '$lib/data-stores/toastStore';
 
 	export let tokenFor: 'POND' | 'MPOND' = 'POND';
-	export let buttonText = 'Add Token';
 
 	async function addTokenToWallet(
 		walletProvider: Eip1193Provider,
 		tokenData: TokenMetadata | undefined
 	) {
-		//TODO: could check from the wallet provider which wallet it associates to and add in functions for each wallet if that is something that we can do
-		if (walletProvider) {
-			try {
-				const added = await walletProvider.request({
-					method: 'wallet_watchAsset',
-					params: {
-						type: 'ERC20',
-						options: {
-							address: tokenData?.address,
-							symbol: tokenData?.currency,
-							decimals: tokenData?.decimal,
-							image: staticImages.MarlinLogo
-
+		if (!$chainStore.isValidChain) {
+			addToast({
+				message: 'You are currently on an unsupported network. Please connect to a valid chain',
+				variant: 'error'
+			});
+		} else {
+			if (walletProvider) {
+				try {
+					const added = await walletProvider.request({
+						method: 'wallet_watchAsset',
+						params: {
+							type: 'ERC20',
+							options: {
+								address: tokenData?.address,
+								symbol: tokenData?.currency,
+								decimals: tokenData?.decimal,
+								image: staticImages.MarlinLogo
+							}
 						}
+					});
+					if (added) {
+						addToast({ message: `${tokenFor} added to wallet successfully!`, variant: 'success' });
+					} else {
+						addToast({
+							message: 'Uh-oh, there seems to be an issue, please try again',
+							variant: 'info'
+						});
 					}
-				});
-				if (added) {
-					console.log('Token added');
-				} else {
-					console.log('Token not added');
+				} catch (error) {
+					console.log(error);
+					addToast({
+						message: 'There seems to be an error. Please try again in some time',
+						variant: 'error'
+					});
 				}
-			} catch (error) {
-				console.log(error);
 			}
 		}
 	}
 </script>
 
 <Button
+	variant="greyFilled"
+	size="small"
+	styleClass="font-medium gap-3"
 	onclick={() => addTokenToWallet($web3WalletStore[0].provider, $chainConfigStore.tokens[tokenFor])}
-	>{buttonText}</Button
+>
+	<span class="text-2xl font-light">+</span> Add {tokenFor}</Button
 >

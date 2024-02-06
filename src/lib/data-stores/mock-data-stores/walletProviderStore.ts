75d945f671444526e2d62d6084e6fb032b0fe677
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-02-06 13:14:42 +0530

    [test][header]: added test for DisconnectWalletModal

--- src/lib/data-stores/mock-data-stores/walletProviderStore.ts
+++ src/lib/data-stores/mock-data-stores/walletProviderStore.ts
@@ -1,5 +1,5 @@
-import type { WalletStore } from '$lib/types/storeTypes';
-import { DEFAULT_WALLET_STORE } from '$lib/utils/constants/storeDefaults';
+import type { ChainStore, WalletStore } from '$lib/types/storeTypes';
+import { DEFAULT_CHAIN_STORE, DEFAULT_WALLET_STORE } from '$lib/utils/constants/storeDefaults';
 import type { WalletState } from '@web3-onboard/core';
 import { writable, type Writable } from 'svelte/store';
 
@@ -23,4 +23,18 @@ export const walletStore = {
     subscribe: walletStoreValue.subscribe,
     set: vi.fn(),
     mockSetSubscribeValue: (value: WalletStore): void => walletStoreValue.set(value)
+};
+
+export const chainConfigStoreValue = writable<{ block_explorer_name: string, block_explorer_url: string }>({ block_explorer_name: 'Block Explorer', block_explorer_url: 'https://blockexplorer.com' });
+export const chainConfigStore = {
+    subscribe: chainConfigStoreValue.subscribe,
+    set: vi.fn(),
+    mockSetSubscribeValue: (value: { block_explorer_name: string, block_explorer_url: string }): void => chainConfigStoreValue.set(value)
+};
+
+export const chainStoreValue: Writable<ChainStore> = writable(DEFAULT_CHAIN_STORE);
+export const chainStore = {
+    subscribe: chainStoreValue.subscribe,
+    set: vi.fn(),
+    mockSetSubscribeValue: (value: ChainStore): void => chainStoreValue.set(value)
 };

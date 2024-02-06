f07b319286005b23484efdbe09b76816636848d0
nayanmarlin <nayan@marlin.org>
2024-02-06 17:43:48 +0530

    fix: some suggested changes

--- src/lib/atoms/timer/Timer.dom.spec.ts
+++ src/lib/atoms/timer/Timer.dom.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, vi } from 'vitest';
 import { cleanup, render } from '@testing-library/svelte';
 import Timer from './Timer.svelte';
-import TestTimer from './TestTimer.svelte';
+import html from 'svelte-htm';
 
 const timerTestId = 'timer-test';
 const delay = (duration: number) => new Promise((resolve) => setTimeout(resolve, duration));
@@ -13,8 +13,8 @@ describe('TimerComponent Component', () => {
 	});
 
 	test('renders without crashing', () => {
-		const { container } = render(Timer);
-		expect(container.firstChild).toBeTruthy();
+		const { getByTestId } = render(Timer);
+		expect(getByTestId('timer')).toBeTruthy();
 	});
 
 	test('does not invoke onTimerEnd callback when countdown is inactive', () => {
@@ -47,36 +47,25 @@ describe('TimerComponent Component', () => {
 	});
 
 	test('timer render with Active slot ', async () => {
-		const { getByTestId } = render(TestTimer, {
-			props: { timerId: timerTestId, endEpochTime: Date.now() / 1000 + 10 }
-		});
-		const activeSlot = getByTestId('timer').firstElementChild?.slot;
+		const { getByTestId } = render(
+			html`<${Timer} timerId=${timerTestId} endEpochTime=${
+				Date.now() / 1000 + 10
+			}><div slot="active" id="active-slot-timer" let:timer>{timer}</div>
+            <div slot="inactive" id="inactive-slot-timer">Timer End</div></${Timer}>`
+		);
+		const activeSlotId = getByTestId('timer').firstElementChild?.id;
 		expect(getByTestId('timer')?.id === timerTestId).toBe(true);
-		expect(activeSlot === 'active').toBeTruthy();
-	});
-
-	test('timer change in every second', async () => {
-		const { getByTestId } = render(TestTimer, {
-			props: { timerId: timerTestId, endEpochTime: Date.now() / 1000 + 10 }
-		});
-		const activeSlot = getByTestId('timer').firstElementChild?.slot;
-		expect(getByTestId('timer')?.id === timerTestId).toBe(true);
-		expect(activeSlot === 'active').toBeTruthy();
-		await delay(1000);
-		expect(getByTestId('timer').firstElementChild?.innerHTML === '8').toBe(true);
-		await delay(1000);
-		expect(getByTestId('timer').firstElementChild?.innerHTML === '7').toBe(true);
-		await delay(1000);
-		expect(getByTestId('timer').firstElementChild?.innerHTML === '6').toBe(true);
+		expect(activeSlotId === 'active-slot-timer').toBeTruthy();
 	});
 
 	test('timer render with Inactive slot ', () => {
-		const { getByTestId } = render(TestTimer, {
-			props: { timerId: timerTestId, endEpochTime: Date.now() / 1000 - 10 }
-		});
-		const activeSlot = getByTestId('timer').firstElementChild?.slot;
+		const { getByTestId } = render(html`<${Timer} timerId=${timerTestId} endEpochTime=${
+			Date.now() / 1000 - 10
+		}><div slot="active" id="active-slot-timer" let:timer>{timer}</div>
+        <div slot="inactive" id="inactive-slot-timer">Timer End</div></${Timer}>`);
+		const inActiveSlotId = getByTestId('timer').firstElementChild?.id;
 		expect(getByTestId('timer')?.id === timerTestId).toBe(true);
-		expect(activeSlot === 'inactive').toBeTruthy();
+		expect(inActiveSlotId === 'inactive-slot-timer').toBeTruthy();
 	});
 
 	test('timer render with onTimerEnd function ', async () => {
@@ -89,7 +78,6 @@ describe('TimerComponent Component', () => {
 				}
 			}
 		});
-		await delay(1000);
 		expect(getByTestId('timer').classList.contains('on-time-end')).toBeFalsy();
 		await delay(1000);
 		expect(getByTestId('timer').classList.contains('on-time-end')).toBeTruthy();

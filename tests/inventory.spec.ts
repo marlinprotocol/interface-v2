501df76b857b5a253a7561b9313f5360a7d50093
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-02-14 14:55:36 +0530

    added inventory test when wallet not connected

---
+++ tests/inventory.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('show connect wallet and no table', async ({ page }) => {
+    await page.goto('/oyster/inventory/', { waitUntil: 'networkidle' });
+
+    const hasHeaderText = await page.textContent('text=My Active Order');
+
+    // Selector that targets any button element
+    const connectWalletButtons = await page.$$('button:has-text("Connect Wallet")');
+
+    // Check if there are two "Connect Wallet" buttons
+    const hasTwoConnectWalletButtons = connectWalletButtons.length === 2;
+
+    console.log('Has two "Connect Wallet" buttons:', hasTwoConnectWalletButtons);
+
+
+    expect(hasHeaderText).toBeTruthy();
+    expect(hasTwoConnectWalletButtons).toBeTruthy();
+});

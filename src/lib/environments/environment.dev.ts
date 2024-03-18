6bb14768d06626752d320d5b89ede8b27b39e265
Souvik Mishra <mishra.souvik911@gmail.com>
2024-03-18 13:52:04 +0530

    added support for arb sepolia on bridge

--- src/lib/environments/environment.dev.ts
+++ src/lib/environments/environment.dev.ts
@@ -9,7 +9,7 @@ export const ENVIRONMENT_DEV: Environment = {
 	valid_chains: { 421614: ARB_SEPOLIA, 421613: ARB_GOERLI, 59140: LINEA_GOERLI },
 	default_chain_id: 421614,
 	supported_chains: {
-		bridge: [421613],
+		bridge: [421613, 421614],
 		oyster: [421613, 59140, 421614],
 		receiver_staking: [421613, 421614],
 		receiver_rewards: [421613, 421614]

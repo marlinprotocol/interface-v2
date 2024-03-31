2c2ecc5e3b495698693fd51f7f5666c6e2b43507
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-03-31 17:13:29 +0530

    fixed bottom links ans buttons added

---
+++ src/lib/components/headerv2/sub-components/MenuItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let iconSVG: string;
+	export let label: string;
+	export let isNavOpen: Boolean;
+</script>
+
+<div class="flex min-w-[24px] px-4 pt-4">
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		{@html iconSVG}
+	</svg>
+	{#if isNavOpen}
+		<p class="ml-4 whitespace-nowrap font-poppins text-[14px] text-[#26272C]">{label}</p>
+	{/if}
+</div>
+
+<!--   ${
+			isNavOpen ? 'visible opacity-100' : 'invisible opacity-0'
+		} -->

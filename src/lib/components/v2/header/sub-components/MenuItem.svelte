fe8604c3edde8a1e9832747383870c3e8cfaf2f4
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-04-02 16:07:11 +0530

    refactor: sidebar bottom menus

--- src/lib/components/v2/header/sub-components/MenuItem.svelte
+++ src/lib/components/v2/header/sub-components/MenuItem.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	export let iconSVG: string;
+	import exp from 'constants';
+
 	export let label: string;
+	export let imgSrc: string;
 	export let isNavOpen: Boolean;
 </script>
 
-<div class="flex min-w-[24px] px-4 pt-4">
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		{@html iconSVG}
-	</svg>
+<li class="flex min-w-[24px] px-4 pt-4">
+	<img src={imgSrc} alt="{label} Icon" />
 	{#if isNavOpen}
 		<p class="ml-4 whitespace-nowrap font-poppins text-[14px] text-[#26272C]">{label}</p>
 	{/if}
-</div>
+</li>
---
+++ src/lib/types/v2/navigation.ts
@@ -0,0 +1,4 @@
+export type MenuItemType = {
+	imgSrc: string;
+	label: string;
+};

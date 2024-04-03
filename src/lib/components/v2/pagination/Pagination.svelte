c72adba3bb7e74264cfc1a8f70f8b3c480ea9a31
nayanmarlin <nayan@marlin.org>
2024-04-03 11:27:06 +0530

    fix: optimized code

--- src/lib/components/v2/pagination/Pagination.svelte
+++ src/lib/components/v2/pagination/Pagination.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
-	import { buttonClasses } from '$lib/atoms/componentClasses';
 	import { staticImages } from '$lib/components/images/staticImages';
 	import { cn } from '$lib/utils/helpers/commonHelper';
 
 	export let activePage: number;
 	export let pageCount: number;
 	export let handlePageChange: (page: number) => void;
-	// export let styleClass = '';
 
 	$: pageArray = Array.from({ length: pageCount }, (_, i) => i + 1);
 	$: isLastPage = activePage === pageArray.length;
 	$: isFirstPage = activePage === 1;
-	$: thirdPageCount =
-		activePage <= 5 ? 3 : pageCount - activePage > 2 ? activePage - 2 : pageCount - 4;
-	$: forthPageCount =
-		activePage > pageCount - 3 ? pageCount - 3 : activePage === 4 ? 4 : activePage - 1;
-	$: fifthPageCount =
-		activePage > pageCount - 3 || activePage < 4
+	$: thirdPageCount = calculateThirdPageCount(activePage, pageCount);
+	$: forthPageCount = calculateForthPageCount(activePage, pageCount);
+	$: fifthPageCount = calculateFifthPageCount(activePage, pageCount);
+
+	function calculateThirdPageCount(activePage: number, pageCount: number): number {
+		return activePage <= 5 ? 3 : pageCount - activePage > 2 ? activePage - 2 : pageCount - 4;
+	}
+
+	function calculateForthPageCount(activePage: number, pageCount: number): number {
+		return activePage > pageCount - 3 ? pageCount - 3 : activePage === 4 ? 4 : activePage - 1;
+	}
+
+	function calculateFifthPageCount(activePage: number, pageCount: number): number {
+		return activePage > pageCount - 3 || activePage < 4
 			? pageCount - 2
 			: activePage === 4
 				? 5
 				: activePage;
+	}
 </script>
 
 <div class="flex items-center">
@@ -61,7 +68,6 @@
 		>
 			1
 		</button>
-
 		<button
 			on:click={() => {
 				activePage <= 3 && handlePageChange(2);
@@ -144,7 +150,6 @@
 			on:click={() => handlePageChange(pageCount)}
 			class={cn(
 				'tex-sm flex h-10 w-10 items-center justify-center border border-[#D9DADE] bg-white font-poppins font-normal text-[#344054]',
-
 				activePage === pageCount && 'bg-[#3840C7] text-white'
 			)}
 		>

f590a34df3d168abbe3e0fbfe3625c70a891d612
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-04-04 16:20:29 +0530

    fix: updated collapse button

--- src/lib/components/v2/buttons/CollapseButton.svelte
+++ src/lib/components/v2/buttons/CollapseButton.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { buttonClasses } from '$lib/atoms/componentClasses';
+	import { buttonClasses } from '$lib/atoms/v2/componentClasses';
 	import Icon from '$lib/atoms/icons/Icon.svelte';
 	import { staticImages } from '$lib/components/images/staticImages';
 	import chevronDown from 'svelte-awesome/icons/chevronDown';
@@ -14,13 +14,12 @@
 	$: chevronIcon = isOpen ? chevronUp : chevronDown;
 </script>
 
-<button
-	data-testid="collapse-button"
-	{id}
-	on:click={onclick}
-	class="{styleClass} {buttonClasses.iconLightBlue}"
-	{disabled}
->
-	<!-- <img src={staticImages.chevronDownIcon}  /> -->
-	<Icon data={chevronIcon} size={12} iconColorClass={disabled ? 'icon-disabled' : 'icon-primary'} />
+<!-- class="{styleClass} {buttonClasses.iconLightBlue}" -->
+<button data-testid="collapse-button" {id} on:click={onclick} {disabled}>
+	<img
+		class="{isOpen ? 'rotate-180' : 'rotate-0'} transform transition duration-200"
+		src={staticImages.chevronDownIcon}
+		alt="Collapse Icon"
+	/>
+	<!-- <Icon data={chevronIcon} size={12} iconColorClass={disabled ? 'icon-disabled' : 'icon-primary'} /> -->
 </button>

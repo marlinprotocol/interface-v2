754ebad71162d7a07b8624dd58bd057f5cd0f1af
nayanmarlin <nayan@marlin.org>
2024-04-05 17:31:10 +0530

    chore: copy paste code from old ui

---
+++ src/lib/atoms/v2/toast/Toast.dom.spec.ts
@@ -0,0 +1,73 @@
+import { cleanup, fireEvent, render, waitForElementToBeRemoved } from '@testing-library/svelte';
+import Toast from './Toast.svelte';
+import { chevronDown } from 'svelte-awesome/icons';
+import type { CommonVariant } from '$lib/types/componentTypes';
+
+const { toastsStore } = await vi.hoisted(
+	() => import('$lib/data-stores/mock-data-stores/mockStores')
+);
+
+const data = {
+	iconColor: 'red',
+	message: 'You have successfully toast your notification',
+	className: 'Default',
+	dismissible: true,
+	iconData: chevronDown,
+	id: 298429842984,
+	timeout: 2000,
+	variant: 'primary' as CommonVariant
+};
+
+beforeAll(() => {
+	vi.mock('$lib/data-stores/toastStore', async () => {
+		return {
+			toastsStore,
+			dismissToast: vi.fn()
+		};
+	});
+});
+
+describe('Toast', () => {
+	afterEach(() => {
+		cleanup();
+		vi.resetAllMocks();
+		toastsStore.dismissToast(298429842984);
+	});
+
+	it('renders no toasts initially', () => {
+		const { queryAllByTestId } = render(Toast);
+		expect(queryAllByTestId('toast')).toHaveLength(0);
+	});
+
+	it('renders a toast when added to the store', () => {
+		toastsStore.mockSetSubscribeValue([data]);
+		const { getByTestId } = render(Toast);
+		const toastElement = getByTestId('toast');
+		const toastClasslist = toastElement.classList;
+		expect(toastElement).toBeTruthy();
+		expect(toastClasslist.contains('toast')).toBe(true);
+		expect(toastElement).toMatchSnapshot();
+	});
+
+	it('fires the onClick event when clicked toast Icon', async () => {
+		toastsStore.mockSetSubscribeValue([data]);
+		const { getByTestId, queryAllByTestId } = render(Toast);
+		const toastBtn = getByTestId('toast-btn');
+		await fireEvent.click(toastBtn);
+		waitForElementToBeRemoved(queryAllByTestId('toast')).then(() =>
+			expect(queryAllByTestId('toast')).toHaveLength(0)
+		);
+	});
+
+	it('renders with toast class', () => {
+		toastsStore.mockSetSubscribeValue([data]);
+		const { getByTestId } = render(Toast);
+		expect(getByTestId('toast').classList.contains('toast')).toBe(true);
+	});
+
+	it('renders with toast position top', () => {
+		toastsStore.mockSetSubscribeValue([data]);
+		const { getByTestId } = render(Toast);
+		expect(getByTestId('toast').classList.contains('toast-top')).toBe(true);
+	});
+});
---
+++ src/lib/atoms/v2/toast/Toast.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Icon from '$lib/atoms/icons/Icon.svelte';
+	import { dismissToast, toastsStore } from '$lib/data-stores/toastStore';
+	import { fade, slide } from 'svelte/transition';
+</script>
+
+{#if $toastsStore.length > 0}
+	<div class="toast toast-end toast-top z-[9999] items-end" data-testid="toast">
+		{#each $toastsStore as toast (toast.id)}
+			<button
+				in:slide
+				out:fade
+				data-testid="toast-btn"
+				on:click={() => dismissToast(toast.id)}
+				class="alert flex w-fit flex-row items-start justify-start gap-1 whitespace-normal rounded px-4 py-2 font-medium shadow-lg {toast.className}"
+			>
+				<div class={'flex h-5 w-5 items-center justify-center'}>
+					<Icon iconColorClass={toast.iconColor} data={toast.iconData} size={18} />
+				</div>
+				<span class="max-w-[330px] grow text-left text-sm">{toast.message}</span>
+			</button>
+		{/each}
+	</div>
+{/if}

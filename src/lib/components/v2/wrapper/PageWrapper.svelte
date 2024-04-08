b7cb21866350f320bc57d4dd093f84af423d2856
Souvik Mishra <mishra.souvik911@gmail.com>
2024-04-08 22:10:34 +0530

    made sidebar animation less jittery and refactored markup

--- src/lib/components/v2/wrapper/PageWrapper.svelte
+++ src/lib/components/v2/wrapper/PageWrapper.svelte
@@ -1,3 +1,6 @@
-<div class="flex items-center justify-center px-4 pb-4 pt-10 lg:px-0" data-testid="page-wrapper">
+<div
+	class="flex max-h-full w-full items-center justify-center overflow-auto px-4 py-10"
+	data-testid="page-wrapper"
+>
 	<slot />
 </div>

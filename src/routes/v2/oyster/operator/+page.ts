5c044f9688294ef74fa8e699f548875403bcd699
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-04-24 14:33:45 +0530

    chore: added routing for oyster routes

---
+++ src/routes/v2/oyster/operator/+page.ts
@@ -0,0 +1,6 @@
+import { ROUTES } from '$lib/utils/constants/v2/urls';
+import { redirect } from '@sveltejs/kit';
+
+export function load() {
+	throw redirect(308, ROUTES.OYSTER_OPERATOR_JOBS_URL);
+}

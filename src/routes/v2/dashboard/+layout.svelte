0a6794ec4d03af33baff319cff5f8e8b794cde39
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-01 19:37:22 +0530

    fix: dashboard show all chains supported

---
+++ src/routes/v2/dashboard/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { setAllowedChainsStore } from '$lib/data-stores/chainProviderStore';
+	import { environment } from '$lib/data-stores/environment';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		setAllowedChainsStore(environment.supported_chains.dashboard);
+	});
+</script>
+
+<slot />

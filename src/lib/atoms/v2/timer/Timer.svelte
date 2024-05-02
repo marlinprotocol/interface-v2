e673a3262603157fff9c5aaf37c64c8bf449654a
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-02 17:48:20 +0530

    chore: added Timer v2 component

---
+++ src/lib/atoms/v2/timer/Timer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { doNothing } from '$lib/utils/helpers/commonHelper';
+
+	export let endEpochTime: number;
+	export let useGivenTime: boolean = false;
+	export let onTimerEnd: () => void = () => {
+		doNothing();
+	};
+	export let timerId: string;
+
+	$: original = useGivenTime ? endEpochTime : Math.floor(endEpochTime - Date.now() / 1000);
+
+	setInterval(() => {
+		if (original > 0) {
+			if (original === 1) {
+				onTimerEnd();
+			}
+			original--;
+		}
+	}, 1000);
+</script>
+
+<div id={timerId} data-testid="timer" class="flex items-center gap-1">
+	{#if original > 0}
+		<slot name="active" timer={original} />
+	{:else}
+		<slot name="inactive" />
+	{/if}
+</div>

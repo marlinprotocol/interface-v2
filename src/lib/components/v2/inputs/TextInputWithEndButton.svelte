2ce96639bdc9059ad2eabc6744c10dcf3bfbd118
nayanmarlin <nayan@marlin.org>
2024-05-03 17:06:00 +0530

    fix: white color leak issue

--- src/lib/components/v2/inputs/TextInputWithEndButton.svelte
+++ src/lib/components/v2/inputs/TextInputWithEndButton.svelte
@@ -11,9 +11,9 @@
 	export let id: string;
 
 	const titleStyleInit =
-		'absolute  cursor-text px-1   font-poppins  text-sm text-[#030115] transition-all ';
-	const onBlurStyle = 'top-[50%]  left-10%  font-medium bg-transparent translate-y-[-50%]';
-	const onFocusStyle = ' left-10%  top-[-10px] font-light bg-white';
+		'absolute cursor-text px-1 font-poppins text-sm text-[#030115] transition-all';
+	const onBlurStyle = 'top-[50%] left-10% font-medium bg-transparent translate-y-[-50%]';
+	const onFocusStyle = 'left-10% top-[-10px] hidden font-light bg-white';
 	let isFocused = false;
 	let titleStyle = onFocusStyle;
 	export let label: string = '';

2ed2aa9fe3c00b074ffc85039e3717c05ba07c7d
Souvik Mishra <mishra.souvik911@gmail.com>
2024-05-16 13:17:31 +0530

    ran script to cleanup

--- src/lib/atoms/tabs/TabList.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let divClass = '';
-</script>
-
-<div class={divClass} data-testid="tab-list">
-	<ul class="-mb-px flex flex-wrap items-center justify-center gap-1" role="tablist">
-		<slot />
-	</ul>
-</div>
--- src/lib/atoms/tabs/Tabs.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let divClass = 'w-full';
-</script>
-
-<div class={divClass} data-testid="tabs">
-	<slot />
-</div>

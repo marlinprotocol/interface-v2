88f404459120381999c9df3762f919bdb54ff6a6
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-23 13:01:10 +0530

    network prompt props handling

--- src/lib/components/prompts/NetworkPrompt.svelte
+++ src/lib/components/prompts/NetworkPrompt.svelte
@@ -1,22 +1,49 @@
 <script lang="ts">
 	import { staticImages } from '$lib/components/images/staticImages';
+	import { cn } from '$lib/utils/helpers/commonHelper';
 	import ChainSwitcher from '../header/sub-components/ChainSwitcher.svelte';
+
+	export let title = 'Unsupported Network';
+	export let description = 'Please switch to one of the chains in the dropdown to continue.';
+	export let variant: 'white' | 'purple' = 'purple';
+	const isVariantWhite = variant === 'white';
+	const imageSrc = isVariantWhite ? staticImages.infoIconWhite : staticImages.infoIconPurple;
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
 	<div
-		class="flex h-[460px] w-130 flex-col items-center justify-center rounded-3xl border border-[#D9DADE] bg-[#3840C7]"
+		class={cn(
+			'flex h-[460px] w-130 flex-col items-center justify-center rounded-3xl border border-[#D9DADE]',
+			isVariantWhite ? 'bg-white' : 'bg-[#3840C7]'
+		)}
 	>
-		<div class="flex h-[84px] w-[84px] items-center justify-center rounded-full bg-white">
-			<img src={staticImages.infoIconPurple} alt="Info Icon" />
+		<div
+			class={cn(
+				'flex h-[84px] w-[84px] items-center justify-center rounded-full',
+				isVariantWhite ? 'bg-[#3840C7]' : 'bg-white'
+			)}
+		>
+			<img src={imageSrc} alt="Info Icon" />
 		</div>
-		<span class="mt-2 text-3xl font-medium text-[#D9DADE]">Unsupported Network</span>
-		<p class="text-md mx-auto mt-2 max-w-[400px] text-center font-light leading-6 text-[#D9DADE]">
-			Please switch to one of the chains in the dropdown to continue.
+		<span
+			class={cn('mt-2 text-3xl font-medium', isVariantWhite ? 'text-[#26272C]' : 'text-[#D9DADE]')}
+			>{title}</span
+		>
+		<p
+			class={cn(
+				'text-md mx-auto mt-2 max-w-[400px] text-center font-light leading-6',
+				isVariantWhite ? 'text-[#A8A8A8]' : 'text-[#D9DADE]'
+			)}
+		>
+			{description}
 		</p>
-		<div class="mt-12 flex w-64 items-center justify-between rounded-full border border-white">
-			<span class="pl-4 font-medium text-[#D9DADE]">Switch</span>
-			<ChainSwitcher />
-		</div>
+		{#if $$slots.cta}
+			<slot name="cta" />
+		{:else}
+			<div class="mt-12 flex w-64 items-center justify-between rounded-full border border-white">
+				<span class="pl-4 font-medium text-[#D9DADE]">Switch</span>
+				<ChainSwitcher />
+			</div>
+		{/if}
 	</div>
 </div>

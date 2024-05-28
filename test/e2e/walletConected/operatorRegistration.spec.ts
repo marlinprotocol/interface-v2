cb1fffad3d9d2bb6d8474568eb2b84f71d517d19
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-05-28 13:55:14 +0530

    operator registration test and refactor

--- test/e2e/walletConected/operatorRegistration.spec.ts
+++ test/e2e/walletConected/operatorRegistration.spec.ts
@@ -1,10 +1,10 @@
 import { MetaMask, testWithSynpress, unlockForFixture } from '@synthetixio/synpress';
-import BasicSetup from '../../wallet-setup/basic.setup';
-// import BasicSetup from '../../wallet-setup/connected.setup';
+// import BasicSetup from '../../wallet-setup/basic.setup';
+import BasicSetup from '../../wallet-setup/connected.setup';
 import { ROUTES } from '../../../src/lib/utils/constants/urls';
 import { MESSAGES } from '../../../src/lib/utils/constants/messages';
 import { loginToMetamask } from '../../helpers/metamask';
-import { Page } from '@playwright/test';
+import { getWalletAddress } from '../../helpers/common';
 
 const test = testWithSynpress(BasicSetup, unlockForFixture);
 const { expect } = test;
@@ -15,80 +15,62 @@ test('Operator Registation', async ({ context, page, metamaskPage, extensionId }
 	await page.goto(ROUTES.OYSTER_OPERATOR_URL, { waitUntil: 'networkidle' });
 
 	const metamask = new MetaMask(context, metamaskPage, BasicSetup.walletPassword, extensionId);
-	await loginToMetamask(metamask, page);
-
-	const hasText = await page.textContent('text=Operator Registration');
-	expect(hasText).toBeTruthy();
-
-	const addressInput = page.getByPlaceholder('Enter your address here');
-	expect(addressInput).toBeDisabled();
-
-	const labelSelector = `label[for="disconnect-wallet-modal"]`;
-	await page.waitForSelector(labelSelector);
-
-	// Click the label
-	await page.click(labelSelector);
-	await page.textContent('text=Your wallet');
-	const walletAddress = await page.getByTestId('wallet-address').innerText();
-
-	await page.getByAltText('Close').click();
-	expect(addressInput).toHaveValue(walletAddress);
-	await page.waitForTimeout(5000);
-	const [unregisterButton] = await page.$$('button:has-text("UNREGISTER")');
-	if (unregisterButton) test.skip();
-
-	const [registerButton] = await page.$$('button:has-text("REGISTER")');
-
-	if (registerButton) {
-		expect(await registerButton.isDisabled()).toBeTruthy();
-		// await page.getByTestId('container-card-body').getByRole('button').first().click();
-		const cpURLInput = page.getByPlaceholder('Paste URL here');
-		expect(cpURLInput).toHaveValue('');
-		expect(cpURLInput).toBeEnabled();
-
-		await cpURLInput.fill(TEST_CP_URL_1);
-
-		await page.waitForSelector('text=Instance Type');
-
-		await page.getByTestId('collapse-button').click();
-
-		expect(await registerButton.isDisabled()).toBeFalsy();
-		await registerButton.click();
-
-		await metamask.confirmTransactionAndWaitForMining();
-	} else {
-		test.skip();
-	}
-});
-
-const getWalletAddress = async (page: Page) => {
-	const labelSelector = `label[for="disconnect-wallet-modal"]`;
-	await page.waitForSelector(labelSelector);
-
-	// Click the label
-	await page.click(labelSelector);
-	await page.textContent('text=Your wallet');
-	const walletAddress = await page.getByTestId('wallet-address').innerText();
-	await page.getByTestId('modal-close-button').nth(1).click();
-	return walletAddress;
-};
-
-test('Operator Unregistration', async ({ context, page, metamaskPage, extensionId }) => {
-	await page.goto(ROUTES.OYSTER_OPERATOR_URL, { waitUntil: 'networkidle' });
-
-	const metamask = new MetaMask(context, metamaskPage, BasicSetup.walletPassword, extensionId);
-	await loginToMetamask(metamask, page);
+	// await loginToMetamask(metamask, page);
 
 	const walletAddress = await getWalletAddress(page);
 	const shortWalletAddress =
 		walletAddress.slice(0, 6) + '...' + walletAddress.slice(walletAddress.length - 6);
 
 	const hasText = await page.textContent(`text=Hello, ${shortWalletAddress}`);
-	console.log({ hasText });
+	expect(hasText).toBeTruthy();
+
+	const [unregisterButton] = await page.$$('button:has-text("UNREGISTER")');
+	if (unregisterButton) test.skip();
+
+	const registerButton = page.getByTestId('page-wrapper').locator('label');
+	registerButton.isDisabled();
+
+	expect(await registerButton.isDisabled()).toBeFalsy();
+	registerButton.click();
+	const cpURLInput = page.getByPlaceholder('Paste URL here');
+	expect(cpURLInput).toHaveValue('');
+	expect(cpURLInput).toBeEnabled();
+
+	await cpURLInput.fill(TEST_CP_URL_1);
+
+	await page.waitForSelector('text=Your control plane URL is loading...');
+	await page.waitForSelector('text=Your control plane URL has been added successfully!');
+
+	const modalRegisterButton = page.getByRole('button', { name: 'Register' });
+
+	expect(await modalRegisterButton.isDisabled()).toBeFalsy();
+	await modalRegisterButton.click();
+
+	await metamask.confirmTransactionAndWaitForMining();
+});
+
+test('Operator Unregistration', async ({ context, page, metamaskPage, extensionId }) => {
+	await page.goto(ROUTES.OYSTER_OPERATOR_URL, { waitUntil: 'networkidle' });
+
+	const metamask = new MetaMask(context, metamaskPage, BasicSetup.walletPassword, extensionId);
+	// await loginToMetamask(metamask, page);
+
+	const walletAddress = await getWalletAddress(page);
+	const shortWalletAddress =
+		walletAddress.slice(0, 6) + '...' + walletAddress.slice(walletAddress.length - 6);
+
+	const hasText = await page.textContent(`text=Hello, ${shortWalletAddress}`);
 	expect(hasText).toBeTruthy();
 
 	const [unregisterButton] = await page.$$('button:has-text("UNREGISTER")');
 	if (!unregisterButton) test.skip();
+
+	expect(await unregisterButton.isDisabled()).toBeFalsy();
+	await unregisterButton.click();
+	await metamask.confirmTransactionAndWaitForMining();
+	await page.waitForSelector(
+		`text=${MESSAGES.TOAST.TRANSACTION.SUCCESS} ${MESSAGES.TOAST.ACTIONS.REMOVE.REMOVED}`
+	);
 });
 
 test('Operator Edit', async ({ context, page, metamaskPage, extensionId }) => {

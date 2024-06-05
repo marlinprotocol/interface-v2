6b86c5f88273701729282ca192635cbf6e9c4074
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-06 02:41:40 +0530

    base url config

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run dev',
-		url: baseURL,
+		url: 'http://127.0.0.1:5173',
 		reuseExistingServer: !process.env.CI
 	}
 });

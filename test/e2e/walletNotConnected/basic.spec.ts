825fca69428a0cb0e754a68e918498ceac9aa388
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-06 02:33:17 +0530

    fix

--- test/e2e/walletNotConnected/basic.spec.ts
+++ test/e2e/walletNotConnected/basic.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('navigating to /  to /dashboard', async ({ page }) => {
 	await Promise.all([
-		await page.goto('/', { waitUntil: 'domcontentloaded' }),
+		await page.goto('/', { waitUntil: 'load' }),
 		await page.waitForURL('/dashboard/')
 	]);
 	const url = page.url();

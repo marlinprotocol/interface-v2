4dc6bb2d8178f5315f1ab05ae458e3cc4c67a40c
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-06 18:58:13 +0530

    testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: 2,
+	retries: 0,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -30,7 +30,7 @@ export default defineConfig({
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL,
-		actionTimeout: 60 * 1000,
+		actionTimeout: 10 * 1000,
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
 	},
--- test/e2e/walletNotConnected/basic.spec.ts
+++ test/e2e/walletNotConnected/basic.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('navigating to /  to /dashboard', async ({ page }) => {
-	await Promise.all([await page.goto('/'), await page.waitForURL('/dashboard/')]);
-	const url = page.url();
-	expect(url.endsWith('/dashboard/')).toBeTruthy();
-});
+// test('navigating to /  to /dashboard', async ({ page }) => {
+// 	await Promise.all([await page.goto('/'), await page.waitForURL('/dashboard/')]);
+// 	const url = page.url();
+// 	expect(url.endsWith('/dashboard/')).toBeTruthy();
+// });
 
 test('demo', async () => {
 	expect(2).toBe(2);

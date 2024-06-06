d10b69b99f5c9c4a7bd12a94dd3df241ff38d6cf
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-07 03:39:19 +0530

    fix

--- test/e2e/walletNotConnected/inventory.spec.ts
+++ test/e2e/walletNotConnected/inventory.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('show connect wallet and no table', async ({ page }) => {
 	await page.goto('http://localhost:5173/oyster/inventory/', { waitUntil: 'domcontentloaded' });
 
+	await page.waitForLoadState();
 	const hasHeaderText = await page.textContent('text=My Active Order');
 
 	// Selector that targets any button element

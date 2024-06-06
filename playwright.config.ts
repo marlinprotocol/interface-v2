02031f0c08ebc89a030a78191d37f2c67ef0f70f
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-07 03:43:17 +0530

    added reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,16 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'line' : 'html',
+	reporter: [
+		[
+			'@estruyf/github-actions-reporter',
+			{
+				title: 'My custom title',
+				useDetails: true,
+				showError: true
+			}
+		]
+	],
 	// reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {

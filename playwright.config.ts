d959715554071f9ad276e97791f2927bccef4d08
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-07 03:29:40 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,6 +65,6 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run dev',
 		url: 'http://localhost:5173',
-		reuseExistingServer: !process.env.CI
+		reuseExistingServer: false
 	}
 });

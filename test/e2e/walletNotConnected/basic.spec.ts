1c769f35ade4f65d63612087a111f4b8636a1c7b
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-06-09 12:49:53 +0530

    uncomment tests

--- test/e2e/walletNotConnected/basic.spec.ts
+++ test/e2e/walletNotConnected/basic.spec.ts
@@ -1,12 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-// test('navigating to /  to /dashboard', async ({ page }) => {
-// 	await Promise.all([await page.goto('/'), await page.waitForURL('/dashboard/')]);
-// 	const url = page.url();
-// 	expect(url.endsWith('/dashboard/')).toBeTruthy();
-// });
-
-test('demo', async () => {
-	console.log('first');
-	expect(2).toBe(2);
+test('navigating to /  to /dashboard', async ({ page }) => {
+	await Promise.all([await page.goto('/'), await page.waitForURL('/dashboard/')]);
+	const url = page.url();
+	expect(url.endsWith('/dashboard/')).toBeTruthy();
 });
--- test/e2e/walletNotConnected/inventory.spec.ts
+++ test/e2e/walletNotConnected/inventory.spec.ts
@@ -2,8 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('show connect wallet and no table', async ({ page }) => {
 	await page.goto('http://localhost:5173/oyster/inventory/', { waitUntil: 'domcontentloaded' });
-	console.log('name', process.env.PUBLIC_SUBGRAPH_KEY);
-	console.log({ name2: process.env.VITE_TEST_CP_URL_2 });
 	await page.waitForLoadState();
 	const hasHeaderText = await page.textContent('text=My Active Order');
 

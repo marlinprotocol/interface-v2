6e366eeaa8506b55e83b8cf8bf5888ff2644a78b
Souvik Mishra <mishra.souvik911@gmail.com>
2024-06-13 17:32:47 +0530

    add prefilled values to inputs and made them disabled

--- src/lib/page-components/kalypso/RegisteredOverview.svelte
+++ src/lib/page-components/kalypso/RegisteredOverview.svelte
@@ -3,10 +3,11 @@
 	import ModalButton from '$lib/atoms/modals/ModalButton.svelte';
 	import AmountInputWithTitle from '$lib/components/inputs/AmountInputWithTitle.svelte';
 	import TextInputWithEndButton from '$lib/components/inputs/TextInputWithEndButton.svelte';
+	import { chainConfigStore } from '$lib/data-stores/chainProviderStore';
+	import { kalypsoStore } from '$lib/data-stores/kalypsoStore';
 	import { walletStore } from '$lib/data-stores/walletProviderStore';
+	import { bigNumberToString } from '$lib/utils/helpers/conversionHelper';
 	import UpdateRewardAddModal from './modals/UpdateRewardAddModal.svelte';
-
-	let rewardsAddress = '';
 </script>
 
 <div class="flex w-full flex-col gap-4">
@@ -16,21 +17,33 @@
 	<div class="flex flex-row items-center justify-center gap-4">
 		<TextInputWithEndButton
 			styleClass="w-full rounded-[100px]"
-			disabled
 			bind:input={$walletStore.address}
 			label="Admin Address"
+			disabled
 		/>
 		<TextInputWithEndButton
 			styleClass="w-full rounded-[100px]"
-			bind:input={rewardsAddress}
+			bind:input={$kalypsoStore.stakingDetails.rewardsAddress}
 			placeholder="Address which will receive rewards"
 			label="Reward address"
+			disabled
 		/>
 	</div>
 	<div class="flex flex-row items-center justify-between gap-4">
 		<div class="flex w-full flex-row items-center gap-4">
-			<AmountInputWithTitle title="Stake" suffix={'POND'} disabled={false} />
-			<AmountInputWithTitle title="Compute" suffix={'%'} disabled={false} onlyInteger />
+			<AmountInputWithTitle
+				title="Stake"
+				inputAmountString={bigNumberToString($kalypsoStore.stakingDetails.stakedAmount)}
+				suffix={$chainConfigStore.tokens.MOCK?.currency}
+				disabled
+			/>
+			<AmountInputWithTitle
+				title="Compute"
+				inputAmountString={bigNumberToString($kalypsoStore.stakingDetails.declaredCompute, 0)}
+				suffix={'%'}
+				disabled={false}
+				onlyInteger
+			/>
 		</div>
 		<div class="flex w-full flex-row items-center justify-end gap-4">
 			<ModalButton
@@ -39,7 +52,7 @@
 				styleClass="font-normal w-32"
 				modalFor="update-reward-address-modal"
 			>
-				Register
+				Update
 			</ModalButton>
 			<Button variant="filled" styleClass="font-normal w-32" size="large">Unregister</Button>
 		</div>
